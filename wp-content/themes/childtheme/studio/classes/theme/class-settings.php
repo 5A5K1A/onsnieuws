aa494f8126475e2c841bcbd08075210f9adbabf1
5A5K1A <saskia.bouten@gmail.com>
2017-03-29 11:27:01 +0200

    fix static call

--- wp-content/themes/childtheme/studio/classes/theme/class-settings.php
+++ wp-content/themes/childtheme/studio/classes/theme/class-settings.php
@@ -1,24 +1,22 @@
 <?php
 
-class Theme_Settings
-{
+class Theme_Settings {
 
-	public function Register() {
+	public static function Register() {
 
 		// setup theme settings
 		if( function_exists('acf_add_options_page') ) {
 
 			acf_add_options_page(array(
-				'page_title' 	=> 'Theme General Settings',
-				'menu_title'	=> 'Theme Settings',
-				'menu_slug' 	=> 'theme-general-settings',
+				'page_title' 	=> __('Instellingen Zendesk ', 'studio'),
+				'menu_title'	=> __('Zendesk', 'studio'),
+				'menu_slug' 	=> 'studio-zendesk',
 				'capability'	=> 'edit_posts',
-				'redirect'		=> true,
-				'position' => '59.5',
-				'icon_url' => 'dashicons-schedule',
+				'redirect'		=> false,
+				'position'      => '57.5',
+				'icon_url'      => 'dashicons-forms',
 			));
 
 		}
 	}
-
 }
