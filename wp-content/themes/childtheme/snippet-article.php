d8649551bdb9c1c23af3d178168c513032efd412
5A5K1A <saskia.bouten@gmail.com>
2017-03-29 21:05:46 +0200

    fix bootstrap collapse panel

--- wp-content/themes/childtheme/snippet-article.php
+++ wp-content/themes/childtheme/snippet-article.php
@@ -5,14 +5,14 @@ $sLabel = sanitize_title( $aData->name );
 
 <div class="panel panel-default">
 	<div class="panel-heading" role="tab" id="faq-<?php echo $sLabel; ?>">
-		<h4 class="panel-title">
-			<a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#<?php echo $sLabel; ?>" aria-expanded="false" aria-controls="<?php echo $sLabel; ?>">
+		<h4 class="panel-title question">
+			<a data-toggle="collapse" data-parent="#<?php echo $this->sSectionId; ?>" href="#<?php echo $sLabel; ?>" aria-expanded="false" aria-controls="<?php echo $sLabel; ?>">
 				<?php echo $aData->name; ?>
 			</a>
 		</h4>
 	</div>
 	<div id="<?php echo $sLabel; ?>" class="panel-collapse collapse" role="tabpanel" aria-labelledby="faq-<?php echo $sLabel; ?>">
-		<div class="panel-body">
+		<div class="panel-body answer">
 			<?php echo apply_filters( 'the_content', $aData->body ); ?>
 		</div>
 	</div>
--- wp-content/themes/childtheme/snippet-section.php
+++ wp-content/themes/childtheme/snippet-section.php
@@ -1,21 +1,15 @@
 <?php
-
+$sSectionId       = $this->aSection['value'];
 
 $oZendeskSection  = new Control_Zendesk_Section();
-$aZendeskArticles = $oZendeskSection->GetSingleSection($this->aSection['value']);
+$aZendeskArticles = $oZendeskSection->GetSingleSection($sSectionId);
 
 
 ?>
 <h2><?php echo $this->aSection['label']; ?></h2>
 
-<?php foreach( (array)$aZendeskArticles as $key => $oArticle ) : ?>
-<div class="article-<?php echo $key; ?>">
-	<?php Template::Render( 'snippet-article', array('oArticle' => $oArticle) ); ?>
+<div class="panel-group" id="section-<?php echo $sSectionId; ?>">
+<?php foreach( (array)$aZendeskArticles as $key => $oArticle ) :
+	Template::Render( 'snippet-article', array('oArticle' => $oArticle, 'sSectionId' => $sSectionId) );
+	endforeach; ?>
 </div>
-
-<?php endforeach; ?>
-
-
-<?php
-
-// p($aZendeskArticles);
