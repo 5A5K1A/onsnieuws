82048993603ef51f9d23523a1f8cf40ec6d5a6e7
5A5K1A <saskia.bouten@gmail.com>
2017-05-12 15:54:22 +0200

    updated styling & some playing around

--- wp-content/themes/childtheme/content-page.php
+++ wp-content/themes/childtheme/content-page.php
@@ -1,6 +1,9 @@
 <main class="articles container">
 
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<?php if( have_posts() ) :
+		while( have_posts() ) :
+			the_post();
+		?>
 
 		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 			<header>
